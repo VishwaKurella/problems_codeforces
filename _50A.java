bad6c702266ac6e357cdd16cd179a65a7da4f9c7
Vishwanath Kurella <vishwa.kurella@icloud.com>
2024-08-06 20:40:22 +0530

    50A

---
+++ _50A.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class _50A {
+    public static void main(String[] args){
+        Scanner S = new Scanner(System.in);
+        int m = S.nextInt();
+        int n = S.nextInt();
+        System.out.println(m*n/2);
+    }
+}
